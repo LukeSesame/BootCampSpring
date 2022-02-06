052ce7cb142a743b51079018eff75d5b6ebcb5bf
LukeSesame <luc.fotso@sesame.com.tn>
2022-02-06 14:42:20 +0100

    Premier projet MVC

---
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -0,0 +1,25 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController {
+	
+	
+	@RequestMapping("/information") //chemin spécifié lors de la saisie dans l'url
+	public String info() { //Nom de la méthode
+	
+		
+		System.out.println("Methode information");
+		return "home/info";//le nom (chemin) de la vue sous template
+	}
+	
+	@RequestMapping("/affichage")
+	public String Afficher() {
+	
+		return "home/affichage";
+	}
+	
+}
